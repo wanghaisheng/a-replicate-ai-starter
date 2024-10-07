06f48ce3f80941229f95ef0e80ba47695054354f
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-10-07 09:35:39 +0530

    editor layout created

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -5,6 +5,11 @@ import { useEffect, useRef } from 'react';
 
 import { useEditor } from '@/features/editor/hooks/use-editor';
 
+import { Footer } from './footer';
+import { Navbar } from './navbar';
+import { Sidebar } from './sidebar';
+import { Toolbar } from './toolbar';
+
 export const Editor = () => {
   const { init } = useEditor();
 
@@ -24,8 +29,20 @@ export const Editor = () => {
 
   return (
     <div className="h-full flex flex-col">
-      <div className="flex-1 h-full bg-muted" ref={containerRef}>
-        <canvas ref={canvasRef} />
+      <Navbar />
+
+      <div className="absolute h-[calc(100%_-_68px)] w-full top-[68px] flex">
+        <Sidebar />
+
+        <main className="bg-muted flex-1 overflow-auto relative flex flex-col">
+          <Toolbar />
+
+          <div className="flex-1 h-[calc(100%_-_124px)] bg-muted" ref={containerRef}>
+            <canvas ref={canvasRef} />
+          </div>
+
+          <Footer />
+        </main>
       </div>
     </div>
   );
