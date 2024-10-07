a60635d2a7bc19d53b0c500ef1b60e4ff015a5df
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-10-07 10:14:46 +0530

    navbar ui setup

---
+++ src/components/hint.tsx
@@ -0,0 +1,33 @@
+import type { PropsWithChildren } from 'react';
+
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
+
+interface HintProps {
+  label: string;
+  side?: 'top' | 'bottom' | 'left' | 'right';
+  align?: 'start' | 'center' | 'end';
+  sideOffset?: number;
+  alignOffset?: number;
+}
+
+export const Hint = ({ label, children, align, side, alignOffset, sideOffset }: PropsWithChildren<HintProps>) => {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={100}>
+        <TooltipTrigger aria-label={label} asChild>
+          {children}
+        </TooltipTrigger>
+
+        <TooltipContent
+          className="text-white bg-slate-800 border-slate-800"
+          side={side}
+          align={align}
+          sideOffset={sideOffset}
+          alignOffset={alignOffset}
+        >
+          <p className="font-semibold capitalize">{label}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
