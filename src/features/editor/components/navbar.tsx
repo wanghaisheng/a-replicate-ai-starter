7c90282ba5c82001d9c55f29a3a924a57cba6569
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-10-07 09:48:52 +0530

    logo added

--- src/features/editor/components/navbar.tsx
+++ src/features/editor/components/navbar.tsx
@@ -1,5 +1,11 @@
 'use client';
 
+import { Logo } from './logo';
+
 export const Navbar = () => {
-  return <nav className="w-full flex items-center p-4 h-[68px] gap-x-8 border-b lg:pl-[34px]">Navbar</nav>;
+  return (
+    <nav className="w-full flex items-center p-4 h-[68px] gap-x-8 border-b lg:pl-[34px]">
+      <Logo />
+    </nav>
+  );
 };
