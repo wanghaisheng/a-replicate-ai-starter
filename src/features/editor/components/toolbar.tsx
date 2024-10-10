0f89e520fba2ea9ae9fdc5957d6d3a9fc962b8cb
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-10-10 10:55:22 +0530

    removed unwanted toolbar options on selecting text

--- src/features/editor/components/toolbar.tsx
+++ src/features/editor/components/toolbar.tsx
@@ -5,6 +5,7 @@ import { RxTransparencyGrid } from 'react-icons/rx';
 import { Hint } from '@/components/hint';
 import { Button } from '@/components/ui/button';
 import type { ActiveTool, Editor } from '@/features/editor/types';
+import { isTextType } from '@/features/editor/utils';
 import { cn } from '@/lib/utils';
 
 interface ToolbarProps {
@@ -16,6 +17,10 @@ interface ToolbarProps {
 export const Toolbar = ({ editor, activeTool, onChangeActiveTool }: ToolbarProps) => {
   const fillColor = editor?.getActiveFillColor();
   const strokeColor = editor?.getActiveStrokeColor();
+
+  const selectedObjectType = editor?.selectedObjects[0]?.type;
+
+  const isText = isTextType(selectedObjectType);
 
   if (editor?.selectedObjects.length === 0) {
     return <div className="shrink-0 h-[56px] border-b bg-white w-full flex items-center overflow-x-auto z-[49] p-2 gap-x-2" />;
@@ -42,37 +47,41 @@ export const Toolbar = ({ editor, activeTool, onChangeActiveTool }: ToolbarProps
         </Hint>
       </div>
 
-      <div className="flex items-center h-full justify-center">
-        <Hint label="Stroke Color" side="bottom" sideOffset={5}>
-          <Button
-            onClick={() => onChangeActiveTool('stroke-color')}
-            size="icon"
-            variant="ghost"
-            className={cn(activeTool === 'stroke-color' && 'bg-gray-100')}
-          >
-            <div
-              aria-hidden
-              className="rounded-sm size-4 border-2 bg-white"
-              style={{
-                borderColor: strokeColor,
-              }}
-            />
-          </Button>
-        </Hint>
-      </div>
+      {!isText && (
+        <div className="flex items-center h-full justify-center">
+          <Hint label="Stroke Color" side="bottom" sideOffset={5}>
+            <Button
+              onClick={() => onChangeActiveTool('stroke-color')}
+              size="icon"
+              variant="ghost"
+              className={cn(activeTool === 'stroke-color' && 'bg-gray-100')}
+            >
+              <div
+                aria-hidden
+                className="rounded-sm size-4 border-2 bg-white"
+                style={{
+                  borderColor: strokeColor,
+                }}
+              />
+            </Button>
+          </Hint>
+        </div>
+      )}
 
-      <div className="flex items-center h-full justify-center">
-        <Hint label="Stroke Width" side="bottom" sideOffset={5}>
-          <Button
-            onClick={() => onChangeActiveTool('stroke-width')}
-            size="icon"
-            variant="ghost"
-            className={cn(activeTool === 'stroke-width' && 'bg-gray-100')}
-          >
-            <BsBorderWidth className="size-4" />
-          </Button>
-        </Hint>
-      </div>
+      {!isText && (
+        <div className="flex items-center h-full justify-center">
+          <Hint label="Stroke Width" side="bottom" sideOffset={5}>
+            <Button
+              onClick={() => onChangeActiveTool('stroke-width')}
+              size="icon"
+              variant="ghost"
+              className={cn(activeTool === 'stroke-width' && 'bg-gray-100')}
+            >
+              <BsBorderWidth className="size-4" />
+            </Button>
+          </Hint>
+        </div>
+      )}
 
       <div className="flex items-center h-full justify-center">
         <Hint label="Bring Forward" side="bottom" sideOffset={5}>
