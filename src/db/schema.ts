d4cb9171e65c880cbe58b4775659aa4b5c483ae4
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-10-18 21:29:13 +0530

    projects schema created

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,6 @@
+import { relations } from 'drizzle-orm';
 import { boolean, integer, pgTable, primaryKey, text, timestamp } from 'drizzle-orm/pg-core';
+import { createInsertSchema } from 'drizzle-zod';
 import type { AdapterAccountType } from 'next-auth/adapters';
 
 export const users = pgTable('user', {
@@ -11,6 +13,10 @@ export const users = pgTable('user', {
   image: text('image'),
   password: text('password'),
 });
+
+export const usersRelations = relations(users, ({ many }) => ({
+  projects: many(projects),
+}));
 
 export const accounts = pgTable(
   'account',
@@ -78,3 +84,32 @@ export const authenticators = pgTable(
     }),
   }),
 );
+
+export const projects = pgTable('project', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  name: text('name').notNull(),
+  userId: text('userId')
+    .notNull()
+    .references(() => users.id, {
+      onDelete: 'cascade',
+    }),
+  json: text('json').notNull(),
+  height: integer('height').notNull(),
+  width: integer('width').notNull(),
+  thumbnailUrl: text('thumbnailUrl'),
+  isTemplate: boolean('isTemplate'),
+  isPro: boolean('isPro'),
+  createdAt: timestamp('createdAt', { mode: 'date' }).notNull(),
+  updatedAt: timestamp('updatedAt', { mode: 'date' }).notNull(),
+});
+
+export const projectsRelations = relations(projects, ({ one }) => ({
+  user: one(users, {
+    fields: [projects.userId],
+    references: [users.id],
+  }),
+}));
+
+export const projectsInsertSchema = createInsertSchema(projects);
