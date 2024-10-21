558a8afdaae4bd2d5b39bf55bc2b028d2e042c30
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-10-21 18:07:59 +0530

    created subscriptiont table

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -112,3 +112,17 @@ export const projectsRelations = relations(projects, ({ one }) => ({
 }));
 
 export const projectsInsertSchema = createInsertSchema(projects);
+
+export const subscriptions = pgTable('subscription', {
+  id: text('id').primaryKey().$defaultFn(createId),
+  userId: text('userId')
+    .notNull()
+    .references(() => users.id, { onDelete: 'cascade' }),
+  subscriptionId: text('subscriptionId').notNull(),
+  customerId: text('customerId').notNull(),
+  priceId: text('priceId').notNull(),
+  status: text('status').notNull(),
+  currentPeriodEnd: timestamp('currentPeriodEnd', { mode: 'date' }),
+  createdAt: timestamp('createdAt', { mode: 'date' }).notNull(),
+  updatedAt: timestamp('updatedAt', { mode: 'date' }).notNull(),
+});
